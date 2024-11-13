bd8888f5b509119eea7d94c8a122653be9eebdb4
kapsarovL <kapsarovlazar@gmail.com>
2024-11-13 22:24:12 +0100

    Setup Testimonials and Pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
+import Pricing from "./sections/Pricing";
 import Faq from "./sections/Faq"; 
+import Testimonials from "./sections/Testimonials";
 
 const App = () => {
   return (
@@ -9,7 +11,9 @@ const App = () => {
       <Header />
       <Hero /> 
       <Features />
+      <Pricing />
       <Faq />
+      <Testimonials />
     </main>
   )
 }
--- src/components/TestimonialItem.jsx
+++ src/components/TestimonialItem.jsx
@@ -1,10 +1,11 @@
 import clsx from "clsx";
+import PropTypes from 'prop-types';
 
 const TestimonialItem = ({ item, containerClassName }) => {
   return (
     <div
     className={clsx(
-        "relative px-14 pb-14 pt-11 after:absolute after:bottom-0 after:right-0 after:h-0.5 after:w-screen after:bg-s2 after:content-[''] max-md:px-0 max-md:pt-11 after:max-md:-right-4"
+        "relative px-14 pb-14 pt-11 after:absolute after:bottom-0 after:right-0 after:h-0.5 after:w-screen after:bg-s2 after:content-[''] max-md:px-0 max-md:pt-11 after:max-md:-right-4",
         containerClassName,
     )}
     >
@@ -26,5 +27,10 @@ const TestimonialItem = ({ item, containerClassName }) => {
     </div>
   )
 }
+
+TestimonialItem.propTypes = {
+  item: PropTypes.object.isRequired,
+  containerClassName: PropTypes.string,
+};
 
 export default TestimonialItem;
