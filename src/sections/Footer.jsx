c7605691a94d25cef0c208e137fcb64168f4618f
kapsarovL <kapsarovlazar@gmail.com>
2024-11-14 00:07:12 +0100

    Setup Download section

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,13 @@
+import {socials} from "../constants/index";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container py-10">
+
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
